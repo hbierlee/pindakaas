787991d3a8e4091f956a9ff5cf4ad5c7773c47c4
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-05-21 13:56:48 +0000

    Raise `Unsatisfiable` exception rather than panic

--- crates/pyndakaas/src/lib.rs
+++ crates/pyndakaas/src/lib.rs
@@ -9,6 +9,11 @@ create_exception!(pindakaas, Unsatisfiable, PyException);
 
 #[pymodule]
 mod pindakaas {
+
+	fn map_unsat(_err: pindakaas::Unsatisfiable) -> PyErr {
+		Unsatisfiable::new_err("constraint was found to be unsatisfiable during encoding")
+	}
+
 	use std::fmt::Display;
 
 	use pindakaas::{
@@ -298,7 +303,7 @@ mod pindakaas {
 				.collect::<PyResult<_>>()?;
 			self.0
 				.add_clause(clause.into_iter().map(|lit| lit.0))
-				.unwrap();
+				.map_err(map_unsat)?;
 			Ok(())
 		}
 
@@ -530,6 +535,7 @@ mod pindakaas {
 
 	#[pymodule]
 	mod solver {
+		use super::map_unsat;
 		use std::{
 			collections::HashMap,
 			sync::Mutex,
@@ -594,7 +600,7 @@ mod pindakaas {
 				let mut guard = self.0.lock().unwrap();
 				guard
 					.add_clause(clause.into_iter().map(|lit| lit.0))
-					.unwrap();
+					.map_err(map_unsat)?;
 				Ok(())
 			}
 
